fe895ebe1379c242c4f2aa0d8a9274f8b41b68ab
EstefaniaMB <estefaniam707@gmail.com>
2018-05-08 16:18:59 -0500

    Base de datos

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express'),
     consolidate = require('consolidate'),
-    hbs = require('handlebars');
+    hbs = require('handlebars'),
+    MongoClient=require('mongodb').MongoClient;
 
-var app = express();
+var app = express(),
+db;
 
 app.engine('hbs', consolidate.handlebars);
 app.set('view engine','hbs');
@@ -10,12 +12,24 @@ app.set('views','./views');
 
 app.use(express.static('public'))
 
+//conectarse a la base de datos
+MongoClient.connect('mongodb://localhost:27017',function(err,client){
+    if(err)throw err;
+    console.log(err);
+    db=client.db('productos');
+    //iniciar el servidor
+    app.listen(1889, function () {
+        console.log('Listening port 1889');
+    });});
+
+
 app.get("/", function (req, res) {
-    res.render('index', {
-        titulo: 'Tonymoly'
+    var maquillaje=db.collection('maquillaje').find();
+    maquillaje.toArray((err,result) =>{
+        res.render('index', {
+            titulo: 'Tonymoly',
+            maquillaje:result
+        }); 
     });
 });
 
-app.listen(3000, function () {
-    console.log('Listening port 3000');
-});
