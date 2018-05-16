83ff172d1f8889e9d7398f9093168003c83fb06e
EstefaniaMB <estefaniam707@gmail.com>
2018-05-16 03:45:14 -0500

    carrito...

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -10,8 +10,8 @@ fetch('http://localhost:1889/productosPorIds?ids=' + arreglo)
         var temp_subtotal = 0;
         var temp_total = 0;
 
-        res.forEach(function (elem, position) {
-            lista.innerHTML += '<li  class="cartListItem"> <div class="cartItem"> <div class="img_item"> <img src="'+ elem.imagen +'" alt=""> </div>'+ '<div class="txt_item"> <h4>' + elem.nombre + '</h4> <p>' + elem.precio +'</p></div><div class="priceItem"><p><strong>$'+ elem.precio;
+        res.forEach(function (elem) {
+            lista.innerHTML += '<li   <div class="cartItem"> <div class="img_item"> <img src="'+ elem.imagen +'" alt=""> </div>'+ '<div class="txt_item"> <h4>' + elem.nombre + '</h4> <p>' + elem.precio +'</p></div><div class="priceItem"><p><strong>$'+ elem.precio;
             temp_subtotal += elem.precio;
             temp_total = temp_subtotal+5;
             subtotal.innerHTML =  '$'+ temp_subtotal+'.00';
